66cdb4e85efaa2e9abf270fc875239b6790c4a40
Jesse Luehrs <doy@tozt.net>
2021-11-06 02:36:29 -0400

    basic skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use async_std::io::WriteExt as _;
+
+async fn async_main() -> anyhow::Result<()> {
+    async_std::io::stdout().write_all(b"hello world\n").await?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match async_std::task::block_on(async_main()) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("nbsh: {}", e);
+            std::process::exit(1);
+        }
+    };
 }
